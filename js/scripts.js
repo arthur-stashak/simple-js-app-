899d00d1d80f3a18d1ec429ad094d0bc4df2f9c9
arthur-stashak <arthur.stashak@outlook.com>
2022-01-19 11:40:00 +0100

    Created loop and conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
+//This creates the pokemon variables
 let pokemonList = [
 { name: 'Pikachu', type: 'electric', height: 2},
 { name: 'Charizard', type: 'fire', height: 6},
 { name: 'Squirtle', type: 'water', height: 3}
 ];
+
+//This is the loop that creates the list of pokemon and their heights
+for (let i=0; i < pokemonList.length; i++){
+  document.write('<p>'+ pokemonList[i].name + ' ' + '(height:' + ' ' + pokemonList[i].height +')' + '<p>');
+}
+
+//This is the conditional that target the largest pokemon.
+for (let i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 5) {
+    document.write(pokemonList[i].name + ' Wow, that is big!');
+  }
+}
